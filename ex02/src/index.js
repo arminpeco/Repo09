2e32544ad96e0fdf15779d7840d5e10157690762
arminpeco <apeco1906@gmail.com>
2021-09-21 23:38:09 +0200

    Added file index.js

---
+++ ex02/src/index.js
@@ -0,0 +1,12 @@
+// Only change code below this line
+function myFunction(myParameter)
+{
+    if(myParameter===true)
+        return console.log("The parameter is true!");
+    return  console.log("The parameter is false!");
+}
+myFunction(true);
+myFunction(false);
+// Only change code above this line 
+
+module.exports=myFunction;
